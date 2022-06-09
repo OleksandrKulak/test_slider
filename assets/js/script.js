09c4f68609dec5a5770ac8f483c63c6fc1e9ffe4
OleksandrKulak <carmelosaha899@gmail.com>
2022-06-09 21:23:54 +0300

    test_slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,63 @@
-var slideIndex = 1;
+window.onload = () => {
+  const slider = document.getElementById('slider');
+  const sliderDots = document.getElementById('slider-dots');
+  const prev = document.getElementById('prev');
+  const next = document.getElementById('next');
+  const dots = sliderDots.querySelectorAll('.dots_item');
+
+  const getActiveElement = () => {
+    const activeSlide = slider.querySelector('.active');
+    const activeSlideDot = sliderDots.querySelector('.active');
+
+    return {activeSlide, activeSlideDot};
+  }
+
+  const setActiveElement = (id) => {
+    const activeSlide = slider.querySelector(`[data-id="${id}"]`);
+    const activeSlideDot = sliderDots.querySelector(`[data-id="${id}"]`);
+
+    activeSlide.classList.add('active');
+    activeSlideDot.classList.add('active');
+  } 
+
+  const slideItem = e => {
+    const {activeSlide, activeSlideDot} = getActiveElement();
+    const activeId = Number(activeSlide.dataset.id);
+
+    activeSlide.classList.remove('active');
+    activeSlideDot.classList.remove('active');
+
+    if (e.target.classList.contains('next')) {
+      const targetId = activeId >= dots.length ? 1 : activeId + 1;
+      setActiveElement(targetId);
+    }
+
+    if (e.target.classList.contains('prev')) {
+     const targetId = activeId === 1 ? dots.length : activeId - 1;
+     setActiveElement(targetId)
+    }
+  }
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const targetId = e.target.dataset.id;
+      const {activeSlide, activeSlideDot} = getActiveElement();
+      const activeId = Number(activeSlide.dataset.id);
+
+      activeSlide.classList.remove('active');
+      activeSlideDot.classList.remove('active');
+      setActiveElement(targetId);
+    });
+  })
+
+  prev.addEventListener('click', slideItem);
+  next.addEventListener('click', slideItem);
+}
+
+
+//
+
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function showSlides(n) {
@@ -32,3 +91,4 @@ function minusSlide() {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+*/
